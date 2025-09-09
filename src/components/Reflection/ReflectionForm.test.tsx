a755d5bdfc1cc07ee3784128fe9865d6e8e8bec8
Leonard Atorough <leotorough@gmail.com>
2025-09-09 10:45:22 +0100

    fix almost all tests for Reflection form

--- src/components/Reflection/ReflectionForm.test.tsx
+++ src/components/Reflection/ReflectionForm.test.tsx
@@ -7,6 +7,8 @@ import {
   testReflection,
   testReflections,
 } from "../../__mocks__/mockReflections";
+import { mockReflections } from "../../data/mockReflections";
+import userEvent from "@testing-library/user-event";
 
 describe("ReflectionForm", () => {
   it("renders correctly when a reflection is passed in", () => {
@@ -36,24 +38,66 @@ describe("ReflectionForm", () => {
 
   it("calls setReflections and adds a new reflection when title is updated", async () => {
     vi.useFakeTimers();
-
     const mockSetReflections = vi.fn();
+
     render(
       <ReflectionForm reflection={null} setReflections={mockSetReflections} />
     );
-    const titleInput = screen.getByRole("textbox", { name: /title/i });
 
+    const titleInput = screen.getByRole("textbox", { name: /title/i });
     // While this would be ideal, it relies on real timers which conflicts with the vitest fake timer
     // await userEvent.type(titleInput, "User inputted Reflection");
 
-    fireEvent.change(titleInput, {
-      target: { value: "User inputted Reflection" },
+    await act(() => {
+      fireEvent.change(titleInput, {
+        target: { value: "User inputted Reflection" },
+      });
     });
 
     await act(() => {
       vi.advanceTimersByTime(500);
     });
+
     expect(mockSetReflections).toHaveBeenCalledTimes(1);
+    const updaterFn = mockSetReflections.mock.calls[0][0];
+    //TODO: Fully understand this line and what its doing
+    const result = updaterFn([]);
+    expect(result).toHaveLength(1);
+    expect(result[0].title).toBe("User inputted Reflection");
+    expect(result[0].content).toBe("");
+
+    vi.useRealTimers();
+  });
+
+  it("calls setReflections and updates reflection when title is changed", async () => {
+    vi.useFakeTimers();
+    const mockSetReflections = vi.fn();
+
+    render(
+      <ReflectionForm
+        reflection={testReflection}
+        setReflections={mockSetReflections}
+      />
+    );
+
+    const user = userEvent.setup({ advanceTimers: vi.advanceTimersByTime });
+    const titleInput = screen.getByRole("textbox", { name: /title/i });
+
+    await user.clear(titleInput);
+    await user.type(titleInput, "New reflection title");
+
+    await act(() => {
+      vi.advanceTimersByTime(500);
+    });
+
+    expect(mockSetReflections).toHaveBeenCalledTimes(1);
+    const updaterFn = mockSetReflections.mock.calls[0][0];
+    //TODO: Fully understand this line and what its doing
+    const result = updaterFn([testReflection]);
+    
+    expect(result).toHaveLength(1);
+    expect(result[0].title).toBe("User inputted Reflection");
+    expect(result[0].content).toBe("");
 
     vi.useRealTimers();
   });
